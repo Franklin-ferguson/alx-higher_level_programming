ed58a52d4c6cab88b452a0a4362ff3811e14cfa9
Franklin-ferguson <hayiborfranklin@gmail.com>
2024-01-24 06:30:39 -0800

    hi

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,10 +3,7 @@
 
 def square_matrix_simple(matrix=[]):
     squared_matrix = []
-    if not matrix:
-        print()
-    else:
-        for line in matrix:
-            squared_matrix.append([c**2 for c in line])
-        return squared_matrix
+    for line in matrix:
+        squared_matrix.append([c**2 for c in line])
+    return squared_matrix
 
