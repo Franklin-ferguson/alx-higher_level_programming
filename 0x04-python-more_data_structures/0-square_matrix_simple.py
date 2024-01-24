c627a44bb29b0334bb793e92c9c330c1706b7343
Franklin-ferguson <hayiborfranklin@gmail.com>
2024-01-24 06:28:29 -0800

    hi

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,14 +2,11 @@
 
 
 def square_matrix_simple(matrix=[]):
+    squared_matrix = []
     if not matrix:
         print()
     else:
-        for row in range(len(matrix)):
-            for item in range(len(matrix[row])):
-                if item != len(matrix[row]) - 1:
-                    endspace = " "
-                else:
-                    endspace = " "
-                print("{:d}".format((matrix[row][item]) ** 2), end=endspace)
-            print()
+        for line in matrix:
+            squared_matrix.append([c**2 for c in line])
+        return squared_matrix
+
