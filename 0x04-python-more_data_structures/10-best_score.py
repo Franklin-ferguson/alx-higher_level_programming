098f503f139fa1fcb8559f562c6735da229e0955
Franklin-ferguson <hayiborfranklin@gmail.com>
2024-01-26 16:33:06 -0800

    hi

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,4 @@
 
 
 def best_score(a_dictionary):
-    return max(a_dictionary, key=a_dictionary.get) if my_dict else None
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
