0113b6f9d8519f6f867cca9323f073b62510a962
root <hayiborfranklin@gmail.com>
2024-04-24 12:49:35 -0700

    hi

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+
+function factoria(a) {
+  if ((isNaN(a)) || (a === 1)) {
+    return 1;
+  } else {
+      return a + factorial(a -1);
+    }
+}
+
+console.log(factorial(parseInt(process.argv[2])));
+
