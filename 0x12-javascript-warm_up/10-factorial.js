1e7f4b0be2cc0bb490a0b0c94aac9407be1483aa
root <hayiborfranklin@gmail.com>
2024-04-24 12:51:25 -0700

    hi

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
+// computes and prints a factorial
 
-
-function factoria(a) {
-  if ((isNaN(a)) || (a === 1)) {
+function factorial (n) {
+  if ((isNaN(n)) || (n === 1)) {
     return 1;
   } else {
-      return a + factorial(a -1);
-    }
+    return n * factorial(n - 1);
+  }
 }
 
 console.log(factorial(parseInt(process.argv[2])));
-
