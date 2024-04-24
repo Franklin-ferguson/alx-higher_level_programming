7fa389ca6b29375f0ae648a22f08b534a56ffb03
root <hayiborfranklin@gmail.com>
2024-04-24 10:53:26 -0700

    hi

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -7,7 +7,7 @@ const hey = 'C is fun';
 for (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {	
-  for (let i = 0; i < process.argv[2]; i++) {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
     console.log(hey);
   }
 }
