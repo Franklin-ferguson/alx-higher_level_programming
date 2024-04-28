d9f07c1acfa71246509619a07119c2bafc648047
root <hayiborfranklin@gmail.com>
2024-04-28 02:24:42 -0700

    hi

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+
+if __name__ = "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+
+    try:
+        conn = MySQLdb.connent(
+                host="localhost",
+                port=3306,
+                user=username,
+                passwd=password,
+                db=db_name,
+                charset="utf8"
+                )
+    except MySQLdb.Error as e:
+        print("Error connecting to dabase: {}".forrmat(e))
+        sys.exit(1)
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+            ORDER BY states.id ASC")
+
+    rows cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+        cur.close()
+        conn.close()
