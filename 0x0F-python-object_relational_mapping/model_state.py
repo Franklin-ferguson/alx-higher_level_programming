c86b485236b2ba04fa5c3d09bfd17a133a4e0a45
root <hayiborfranklin@gmail.com>
2024-05-01 06:32:05 -0700

    hi

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,12 +5,8 @@ Link a class to a table in a database with SQLAlchemy
 """
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy import Column, Integer, String
-
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
 Base = declarative_base()
 
 class State(Base):
