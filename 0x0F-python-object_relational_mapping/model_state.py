6c9717a63a191a62ba56a30ad566c2071b866b44
root <hayiborfranklin@gmail.com>
2024-05-01 06:34:47 -0700

    hi

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,6 +11,13 @@ Base = declarative_base()
 
 
 class State(Base):
+    
+     """State class
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The State id of the class
+        name (str): The State name of the class
+    """
 
     __tablename__ = 'states'
 
