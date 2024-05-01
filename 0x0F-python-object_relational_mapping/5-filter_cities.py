b5f42e8cb2f4f9dd0c5de5dc2ca390a48d949ad0
root <hayiborfranklin@gmail.com>
2024-05-01 05:44:51 -0700

    task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
+"""
+This script  takes in the name of a state
+as an argument and lists all cities of that
+state, using the database `hbtn_0e_4_usa`.
+"""
 
 import MySQLdb as db
 from sys import argv
 
 if __name__ == "__main__":
+    """
+    Access to the database and get the cities
+    from the database.
+    """
 
     db_connect = db.connect(host="localhost", port=3306,
                             user=argv[1], passwd=argv[2], db=argv[3])
 
-    db_cursor = db_connect.cursor()
-
-    db_cursor.execute("""
+    with db_connect.cursor() as db_cursor:
+        db_cursor.execute("""
             SELECT
                 cities.id, cities.name
             FROM
@@ -26,8 +34,7 @@ if __name__ == "__main__":
         """, {
             'state_name': argv[4]
         })
+        rows_selected = db_cursor.fetchall()
 
-    rows = db_cursor.fetchall()
-
-    if rows is not None:
+    if rows_selected is not None:
         print(", ".join([row[1] for row in rows_selected]))
