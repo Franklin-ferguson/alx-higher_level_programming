3256ac5e82983c178b9d5027f357fbdc36db13fe
root <hayiborfranklin@gmail.com>
2024-05-01 06:21:38 -0700

    hi

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,9 +4,7 @@
 Link a class to a table in a database with SQLAlchemy
 """
 
-import sys
-form modle_state import Base, State
-from sqlalchemy import (create_engine), Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -19,9 +17,3 @@ class State(Base):
 
     name = Column(String(128),nullable=False)
 
-
-
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-
-    Base.metadata.create_all(engine)
