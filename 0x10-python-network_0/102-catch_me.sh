501d765464a5cc9d18101e4eb6cf0d57baa0adba
root <hayiborfranklin@gmail.com>
2024-05-09 14:00:00 -0700

    hi

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message
+# makes a request to 0.0.0.0:5000/catch_me that causes the server
 curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
