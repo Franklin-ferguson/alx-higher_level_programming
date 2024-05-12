3dd5da9f8f500ee6f8cc61b6e3522bead7f32bdd
root <hayiborfranklin@gmail.com>
2024-05-12 10:46:17 -0700

    hi

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,8 +7,7 @@ import requests
 
 if __name__ == "__main__":
 
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-            argv[2], argv[1])
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
 
     r = requests.get(url)
 
@@ -16,9 +15,12 @@ if __name__ == "__main__":
 
     try:
         for i in range(10):
-            print("{}: {}".format( commit[i].get("sha"),
-                commit[i].get("commit").get("author").get("name")))
+            print(
+                "{}: {}".format(
+                    commit[i].get("sha"),
+                    commit[i].get("commit").get("author").get("name"),
+                )
+            )
 
     except IndexError:
         pass
-
