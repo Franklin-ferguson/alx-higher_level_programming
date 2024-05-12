b82184a5b3d9fc569b9bf5733c284c4c4b109064
root <hayiborfranklin@gmail.com>
2024-05-12 02:54:44 -0700

    task 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,14 +7,15 @@ script  fetches https://alx-intranet.hbtn.io/status
 from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
 
-url = 'https://alx-intranet.hbtn.io/status'
-req = Request(url)
-with urlopen(req) as response:
-    status_code = response.getcode()
-    the_page = response.read().decode()
-    lines = the_page.splitlines()
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    req = Request(url)
+    with urlopen(req) as response:
+        status_code = response.getcode()
+        the_page = response.read().decode()
+        lines = the_page.splitlines()
 
-    print("{}".format(status_code))
-    print("\nBody: ")
-    for line in lines:
-        print("{}".format(line))
+        print("{}".format(status_code))
+        print("\nBody: ")
+        for line in lines:
+            print("{}".format(line))
