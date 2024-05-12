ea78dda377636d6ff856ba33a7236094deec60ef
root <hayiborfranklin@gmail.com>
2024-05-12 07:26:51 -0700

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""script that takes in a URL, sends a request to the URL and displays 
-   the body of the response
+"""script that takes in a URL, sends a request to the URL and displays
 """
-
 from urllib.request import Request, urlopen
 from sys import argv
 from urllib.error import HTTPError, URLError
+
 
 if __name__ == "__main__":
 
@@ -15,6 +14,5 @@ if __name__ == "__main__":
     try:
         with urlopen(req) as response:
             print(response.read().decode("ascii"))
-
     except HTTPError as error:
         print("Error code: {}".format(error.code))
