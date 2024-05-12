eb80363eff350c89e06482f3b028f887df6c7daa
root <hayiborfranklin@gmail.com>
2024-05-12 07:23:57 -0700

    task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL and displays 
+   the body of the response
+"""
+
+from urllib.request import Request, urlopen
+from sys import argv
+from urllib.error import HTTPError, URLError
+
+if __name__ == "__main__":
+
+    url = argv[1]
+
+    req = Request(url)
+    try:
+        with urlopen(req) as response:
+            print(response.read().decode("ascii"))
+
+    except HTTPError as error:
+        print("Error code: {}".format(error.code))
